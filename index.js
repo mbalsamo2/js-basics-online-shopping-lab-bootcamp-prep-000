e66a70827c7f85b1106e21895e6a6f99a594078a
Maria Balsamo <mia.balsamo@gmail.com>
2017-12-06 18:55:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -88,5 +88,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  
+  if (cardNumber === undefined) {
+    console.log("Sorry, we don't have a credit card on file for you.");
+  }
 }
